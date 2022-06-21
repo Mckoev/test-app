28afed69f0ebf310da4fca77d72f2db84d5ce774
Mckoev <mckoev@yandex.ru>
2022-06-21 17:39:41 +0300

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css'
 import Home from './img/Sidebar/Home.svg'
+import Exit from './img/Sidebar/Exit.svg'
+import Chat from './img/Sidebar/Chat.svg'
+import Market from './img/Sidebar/Market.svg'
+import Search from './img/Sidebar/Search.svg'
+import Settings from './img/Sidebar/Settings.svg'
 
 function App() {
   return (
     <div className="App">
       hi
       <img src={Home}></img>
+      <img src={Exit}></img>
+      <img src={Chat}></img>
+      <img src={Market}></img>
+      <img src={Search}></img>
+      <img src={Settings}></img>
       <div>Home</div>
     </div>
   )
